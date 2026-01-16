358d76480d7bef9dd5de112e2d25187f1faf99f1
SAMIR-MALEK <samir.malek@univ-bba.dz>
2026-01-16 20:53:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -237,8 +237,7 @@ if st.session_state.logged_in:
     if st.session_state.mode == "view":
         # مسح الكاش وإعادة تحميل البيانات الطازجة
         clear_cache_and_reload()
-        time.sleep(3) 
-
+        
 
         df_memos_fresh = load_memos()
         
