32d73b2fdee246ec463fa27a725f9da27889c336
SAMIR-MALEK <samir.malek@univ-bba.dz>
2026-01-16 20:51:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -237,6 +237,10 @@ if st.session_state.logged_in:
     if st.session_state.mode == "view":
         # مسح الكاش وإعادة تحميل البيانات الطازجة
         clear_cache_and_reload()
+        import time
+        time.sleep(3) 
+
+
         df_memos_fresh = load_memos()
         
         note_number = str(s1.get('رقم المذكرة', '')).strip()
