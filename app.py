c80771cfabfbf5161447f89ef248f64bc9c1835a
SAMIR-MALEK <samir.malek@univ-bba.dz>
2026-02-13 21:26:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1058,7 +1058,7 @@ elif st.session_state.user_type == "student":
                         if not phone_ok:
                             error_details.append(f"الهاتف الحالي ('{s_phone}') غير صالح: {phone_msg}")
                         if not nin_ok:
-                            error_details.append(f"أدخل الرقم الوطني (18 رقما)")
+                            error_details.append(f"أدخل رقم التعريف الوطني (18 رقما)")
                         
                         st.session_state.student1 = result
                         st.session_state.profile_user_temp = result
